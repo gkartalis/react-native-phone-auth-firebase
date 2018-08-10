c0dab5fe465e2c72f59f3189e7ce68268df35b77
gkartalis <g.kartalis7@gmail.com>
2018-08-10 13:35:42 +0300

    RNFirebaseAuth Configuration for Android

--- android/app/src/main/java/com/reactnativephoneauthexample/MainApplication.java
+++ android/app/src/main/java/com/reactnativephoneauthexample/MainApplication.java
@@ -3,10 +3,13 @@ package com.reactnativephoneauthexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseAuthPackage(),
       );
     }
 
